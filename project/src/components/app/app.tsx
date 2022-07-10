d5984a208a0f384c1a5a52e7981ddf6d2eb3a76e
Vadim <rootfig@gmail.com>
2022-07-10 18:21:25 +0300

    Добавил компонент main

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,14 @@
+import MainScreen from '../../pages/main-screen/main-screen';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return(
+    <MainScreen
+      filmCardCount='12'
+      releaseDate='2014'
+      genre='Drama'
+      title='The Grand Budapest Hotel poster'
+      poster='img/the-grand-budapest-hotel-poster.jpg'
+    />);
 }
 
 export default App;
