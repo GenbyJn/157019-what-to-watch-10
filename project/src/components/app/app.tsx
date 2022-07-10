759399bdb83ebbf7127c3584b184ad088e954613
Vadim <rootfig@gmail.com>
2022-07-10 19:20:33 +0300

    Добавил компоненты страниц приложения

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,14 +1,12 @@
 import MainScreen from '../../pages/main-screen/main-screen';
 
-function App(): JSX.Element {
-  return(
-    <MainScreen
-      filmCardCount='12'
-      releaseDate='2014'
-      genre='Drama'
-      title='The Grand Budapest Hotel poster'
-      poster='img/the-grand-budapest-hotel-poster.jpg'
-    />);
-}
+const App = (): JSX.Element => (
+  <MainScreen
+    filmCardCount='12'
+    releaseDate='2014'
+    genre='Drama'
+    title='The Grand Budapest Hotel poster'
+    poster='img/the-grand-budapest-hotel-poster.jpg'
+  />);
 
 export default App;
