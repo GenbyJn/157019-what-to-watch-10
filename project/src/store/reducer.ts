4b8445fc8087f6ad5d6eeb7bd0381f466c4ab97a
Vadim <rootfig@gmail.com>
2022-08-21 16:37:51 +0300

    Работа с cAT

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,25 +1,32 @@
 import { createReducer } from '@reduxjs/toolkit';
 import films from '../mocks/films';
 import { Film } from '../types/film';
-import { DEFAULT_GENRE, FILMS_COUNT } from '../utils/common';
-import { changeGenre, getFilms, showMoreFilms } from './action';
+import { DEFAULT_GENRE, FILMS_COUNT, AuthorizationStatus } from '../utils/common';
+import { changeGenre, showMoreFilms, loadFilms, requireAuthorisation } from './action';
 
 type InitialStateTypes = {
   films: Film []
   genre: string
   filmsCount: number
+  authorizationStatus: AuthorizationStatus,
+  error: string | null
 }
 
 const initialState: InitialStateTypes = {
   films: films,
   genre: DEFAULT_GENRE,
   filmsCount: FILMS_COUNT,
+  authorizationStatus: AuthorizationStatus.Unknown,
+  error: null
 };
 
 const reducer = createReducer(initialState, (builder) => {
   builder
-    .addCase(getFilms, (state, action) => {
-      state.films = action.payload.films;
+    .addCase(loadFilms, (state, action) => {
+      state.films = action.payload;
+    })
+    .addCase(requireAuthorisation, (state, action) => {
+      state.authorizationStatus = action.payload;
     })
     .addCase(changeGenre, (state, action) => {
       state.genre = action.payload;
@@ -27,9 +34,6 @@ const reducer = createReducer(initialState, (builder) => {
     .addCase(showMoreFilms, (state) => {
       state.filmsCount += FILMS_COUNT;
     });
-  // .addCase(setFilmsCount, (state) => {
-  //   state.filmsCount = FILMS_COUNT;
-  // })
 });
 
 export default reducer;
