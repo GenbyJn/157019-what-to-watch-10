2e29984ff732faa23e9fdaabf7b4f8a9c9bd8e2e
Vadim <rootfig@gmail.com>
2022-09-01 21:46:34 +0300

    Исправление по критению Б16

--- project/src/utils/urls.ts
+++ project/src/utils/urls.ts
@@ -3,9 +3,6 @@ import { RouteName } from './common';
 export const getFilmUrl = (id: string | number): string =>
   `/${RouteName.Film.name}/${id}`;
 
-export const getGenreUrl = (genreName: string): string =>
-  `/${RouteName.Genre.name}/${genreName.toLowerCase()}`;
-
 export const getPlayerUrl = (id: string | number): string =>
   `/${RouteName.Player.name}/${id}`;
 
